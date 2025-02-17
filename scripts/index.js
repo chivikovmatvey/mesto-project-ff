2597f57871028a07ba98dca5263acf44631c24a7
chivikovmatvey <chivikovm@gmail.com>
2025-02-17 14:24:31 +0300

    changing the display approach and correcting deletions

--- scripts/index.js
+++ scripts/index.js
@@ -3,18 +3,22 @@ const cardElemeent = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const cardList = document.querySelector('.places__list')
 // @todo: Функция создания карточки
-function addCard (arrCards) {
+function createCard (cardData, deleteCallback) {
+  const card = cardElemeent.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__image').src = cardData.link;
+  card.querySelector('.card__title').textContent= cardData.name
+  card.querySelector('.card__delete-button').addEventListener('click', deleteCallback)
+  return card
+}
+// @todo: Функция удаления карточки
+function delCard(event){
+  event.target.closest('.card').remove();
+}
+// @todo: Вывести карточки на страницу
+function addCard(arrCards) {
   arrCards.forEach(item => {
-    const card = cardElemeent.querySelector('.card').cloneNode(true);
-    card.querySelector('.card__image').src = item.link;
-    card.querySelector('.card__title').textContent= item.name
-    card.querySelector('.card__delete-button').addEventListener('click', delCard)
+    const card = createCard(item, delCard);
     cardList.append(card);
   });
 }
-// @todo: Функция удаления карточки
-function delCard(){
-  document.querySelector('.card__delete-button').closest('.card').remove();
-}
-// @todo: Вывести карточки на страницу
 addCard(initialCards);
